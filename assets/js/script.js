f89949a8bf8f5b87f3221cafa41f941d47017015
sm3131 <sm2683@nau.edu>
2021-11-30 16:47:52 -0700

    condensed code to select textareas on save clicks, fixed local storage bugs, no longer overwrites storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ var currentDate = $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 
 var tasksArray = [
-    "",
+    "", 
     "",
     "",
     "",
@@ -13,26 +13,46 @@ var tasksArray = [
     ""
 ]
 
+
+
 $(".save").on("click", saveClick);
 $(document).ready(reloadTasks)
 
 function saveClick(event) {
+    //debugger;
     if(event.target.matches(".save")) {
         var clickTarget= $(event.target).attr("id");
-    }
-    console.log(clickTarget);
+        var textAreaContent = $("#" + clickTarget).prev().val();
+        var textKey = $("#" + clickTarget).prev().attr("id");
 
-    textId = clickToText(clickTarget);
-    console.log($("#" + textId).val())
+        // var tasksObj = {
+        //     textKey: textAreaContent
+        // };
+
+        // $(tasksArr).push(tasksObj);
+       
+        console.log(textAreaContent);
+        console.log(textKey);
+
+    }
+
+
+    var storeTasksArr = getStoredItems ();
+
+    //console.log(clickTarget);
+
+    // textId = clickToText(clickTarget);
+    // console.log($("#" + textId).val())
 
     arrayIndex = clickToIndex(clickTarget)
-    tasksArray[arrayIndex] = $("#" + textId).val()
+    storeTasksArr[arrayIndex] = textAreaContent;
     console.log(tasksArray);
 
-    localStorage.setItem("calendarTasks", JSON.stringify(tasksArray));
+    localStorage.setItem("calendarTasks", JSON.stringify(storeTasksArr));
 }
 
 function reloadTasks() {
+
     var savedTasks = localStorage.getItem("calendarTasks")
 
     if(!savedTasks) {
@@ -47,83 +67,96 @@ function reloadTasks() {
 
 };
 
-function clickToText(clickTask) {
-    switch (clickTask) {
-        case "click9":
-            text = "task9"
-            break;
+function getStoredItems () {
+    //debugger;
+    var storedItems = localStorage.getItem("calendarTasks")
 
-        case "click10":
-            text = "task10"
-            break; 
+    if(!storedItems) {
+        return false;
+    }
 
-        case "click11":
-            text = "task11"
-            break;
+    storedItems = JSON.parse(storedItems);
 
-        case "click12":
-            text = "task12"
-            break;
+    return storedItems;
+}
 
-        case "click1":
-            text = "task1"
-            break;
+// function clickToText(clickTask) {
+//     switch (clickTask) {
+//         case "click0":
+//             text = "task0"
+//             break;
 
-        case "click2":
-            text = "task2"
-            break;
+//         case "click1":
+//             text = "task1"
+//             break; 
 
-        case "click3":
-            text = "task3"
-            break;
+//         case "click2":
+//             text = "task2"
+//             break;
 
-        case "click4":
-            text = "task4"
-            break;
+//         case "click3":
+//             text = "task3"
+//             break;
 
-        case "click5":
-            text = "task5"
-            break;
-    };
-    return(text);
-    };
+//         case "click4":
+//             text = "task4"
+//             break;
+
+//         case "click5":
+//             text = "task5"
+//             break;
+
+//         case "click6":
+//             text = "task6"
+//             break;
+
+//         case "click7":
+//             text = "task7"
+//             break;
+
+//         case "click8":
+//             text = "task8"
+//             break;
+//     };
+//     return(text);
+//     };
 
 
     function clickToIndex(clickIndex) {
         switch(clickIndex) {
-            case "click9":
+            case "click0":
                 clickArrIndex = 0
                 break;
 
-            case "click10":
+            case "click1":
                 clickArrIndex = 1
                 break;
 
-            case "click11":
+            case "click2":
                 clickArrIndex = 2
                 break;
 
-            case "click12":
+            case "click3":
                 clickArrIndex = 3
                 break;
 
-            case "click1":
+            case "click4":
                 clickArrIndex = 4
                 break;
 
-            case "click2":
+            case "click5":
                 clickArrIndex = 5
                 break;
 
-            case "click3":
+            case "click6":
                 clickArrIndex = 6
                 break;
 
-            case "click4":
+            case "click7":
                 clickArrIndex = 7
                 break;
 
-            case "click5":
+            case "click8":
                 clickArrIndex = 8
                 break;
         }
